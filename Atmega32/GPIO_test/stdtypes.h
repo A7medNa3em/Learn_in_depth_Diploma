6bdc39f4f81f9ed0d42e124aa8b0e36b373ca546
ahmednaaeem <ahmednaaeem99@gmail.com>
2022-11-27 06:15:56 +0200

    interface

---
+++ Atmega32/GPIO_test/stdtypes.h
@@ -0,0 +1,19 @@
+#ifndef STDTYPES_H
+#define STDTYPES_H
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned int uint32;
+
+typedef char int8;
+typedef short int int16;
+typedef int int32;
+
+typedef enum
+{
+    ERROR,
+    SUCESS,
+
+} StdReturn_t;
+
+#endif
